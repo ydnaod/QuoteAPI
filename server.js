1c8e06291063dcc652cff7ea93aa3f9e57c8aedb
ydnaod <65431123+ydnaod@users.noreply.github.com>
2020-12-08 16:05:21 -0500

    add get request for random quote

--- server.js
+++ server.js
@@ -6,5 +6,15 @@ const { getRandomElement } = require('./utils');
 
 const PORT = process.env.PORT || 4001;
 
+const quoteRouter = express.Router();
+app.use('/api/quotes', quoteRouter);
+
+quoteRouter.get('/random', (req, res, next) => {
+    res.send(getRandomElement(quotes));
+})
+
 app.use(express.static('public'));
+app.listen(PORT, () => {
+    console.log('listening on ' + PORT)
+});
 
