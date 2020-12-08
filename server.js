989af1df00221ca0642b981d0b31ad389ebe1bf2
ydnaod <65431123+ydnaod@users.noreply.github.com>
2020-12-08 16:40:41 -0500

    add get request by author

--- server.js
+++ server.js
@@ -10,11 +10,29 @@ const quoteRouter = express.Router();
 app.use('/api/quotes', quoteRouter);
 
 quoteRouter.get('/random', (req, res, next) => {
-    res.send(getRandomElement(quotes));
+    res.send({ quote: getRandomElement(quotes)});
+})
+
+quoteRouter.get('/', (req, res, next) => {
+    if(req.query.person){
+        let arrayToSend = [];
+        console.log(req.query.person);
+        quotes.forEach(quote => {
+        if(quote.person === req.query.person){
+            arrayToSend.push(quote);
+        }
+    })
+    res.send({quotes: arrayToSend});
+    }
+    else{
+        arrayToSend = quotes.map(quote => quote);
+        res.send({quotes: arrayToSend});
+    }
 })
 
 app.use(express.static('public'));
 app.listen(PORT, () => {
     console.log('listening on ' + PORT)
 });
+
 
